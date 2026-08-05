a408664ca60a71fdde13600f4a91062ae3175a92
Erik <mayilyane9@gmail.com>
2026-08-05 13:52:44 +0200

    Integrating Google Gemini

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from google import genai
+import os
+
+load_dotenv()
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = FastAPI()
 
@@ -14,8 +20,15 @@ app.add_middleware(
 class ChatRequest(BaseModel):
   message: str
 
+def ask_gemini(prompt: str) -> str:
+  response = client.models.generate_content(
+    model="gemini-3.6-flash",
+    contents=prompt
+  )
+  return response.text
+
 @app.post("/chat")
 def chat(request: ChatRequest):
-    return {
-      "reply" : "Backend is connected! congratulations!"
-    }
+    user_message = request.message
+    reply = ask_gemini(user_message)
+    return { "reply": reply }
